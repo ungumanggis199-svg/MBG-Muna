aac348bbfaccfe96df513f869a05f620b2404f45
ungumanggis199-svg <ungumanggis199@gmail.com>
2026-06-15 15:14:37 +0800

    Create list-sppg.js

---
+++ api/list-sppg.js
@@ -0,0 +1,47 @@
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({
+      success: false,
+      message: "Method tidak diizinkan."
+    });
+  }
+
+  const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxl7u7NeBrpaXxylVCRmKqhIapX2nRRZN0l5vRu2mDwP3CkJBmRDUopBmbiRoAix_YB/exec";
+
+  try {
+    const gasResponse = await fetch(APPS_SCRIPT_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain;charset=utf-8"
+      },
+      body: JSON.stringify({
+        action: "getListSPPG"
+      })
+    });
+
+    const text = await gasResponse.text();
+
+    let result;
+
+    try {
+      result = JSON.parse(text);
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Respons Apps Script bukan JSON.",
+        detail: text.slice(0, 500),
+        data: []
+      });
+    }
+
+    return res.status(200).json(result);
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Gagal mengambil List SPPG.",
+      detail: error.message,
+      data: []
+    });
+  }
+}
